6baa99f9f2cac1a438500bac3988908c6123778f
RedwanIbneSerajKhan <rikhan@buffalo.edu>
2018-10-01 21:05:02 -0400

    -Enabled feature of displaying city/town in userRequestResponse -Prepared the database model -enabled feature of extracting info from database when origin and destination is the same -created superuser in admin -enabled feature to visualize database from superuser

--- MyWeather/models.py
+++ MyWeather/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class MyWeatherDB(models.Model):
+
+
+    City =  models.CharField(max_length=50)
+    Origin = models.CharField(max_length=50)
+    Destination  = models.CharField(max_length=50)
+
+    Latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    Longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    Humidity = models.FloatField()
+    Pressure = models.FloatField()
+    Temperature = models.FloatField()
+
+    Icon = models.CharField(max_length=20)
+    Description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return u'%s %s %s %s' % (self.City,self.Origin, self.Destination, self.Description)
--- MyWeather/views.py
+++ MyWeather/views.py
@@ -6,6 +6,8 @@ import googlemaps
 import requests
 import json
 import reverse_geocoder as rg
+
+from .models import MyWeatherDB #Importing the model of database that we made in model.py
 # Create your views here.
 
 def myWeather(request):
@@ -16,55 +18,94 @@ def userRequest(request):
     if request.method == "POST":
          origin_place = request.POST['origin_place']
          destination_place = request.POST['destination_place']
+         #--------------------------REFERENCE---------------------------------------
+         # https://djangobook.com/django-models/
+         # https://djangobook.com/django-models-basic-data-access/
 
-         #MAKING GOOGLE CLIENT
+         weather_db_rec = MyWeatherDB.objects.filter(Origin = origin_place, Destination = destination_place)
 
-         gmapclient = googlemaps.Client(key= 'AIzaSyAwx5bqxuLiHc4vyv04xkQp9YGGy1L4xVA')
+         if len(weather_db_rec)> 0:
+             weather_points = []
+             for rec in weather_db_rec:
+                 weather_point = {
+                     'origin': rec.Origin,
+                     'destination': rec.Destination,
+                     'city_name': rec.City,
+                     'latitude': float(rec.Latitude),
+                     'longitude': float(rec.Longitude),
+                     'temperature': rec.Temperature,
+                     'humidity': rec.Humidity,
+                     'pressure': rec.Pressure,
+                     'description': rec.Description,
+                     'icon': rec.Icon
+                 }
+                 weather_points.append(weather_point)
 
-         #USING GOOGLE CLIENT FOR CALLING DIRECTION API TO GET A LIST OF ROUTES FOR TRAVELLING
-         dirs_result = gmapclient.directions(origin_place, destination_place,mode="driving")
+             context = {'weather_info': json.dumps(weather_points)}
+             return render(request, 'MyWeather/userRequestResponse.html', context)
+         else:
+             #MAKING GOOGLE CLIENT
+             #https://djangobook.com/django-models/
+             #https://djangobook.com/django-models-basic-data-access/
+             gmapclient = googlemaps.Client(key= 'AIzaSyAwx5bqxuLiHc4vyv04xkQp9YGGy1L4xVA')
 
-         steps_in_leg = dirs_result[0]['legs'][0]['steps']
+             #USING GOOGLE CLIENT FOR CALLING DIRECTION API TO GET A LIST OF ROUTES FOR TRAVELLING
+             dirs_result = gmapclient.directions(origin_place, destination_place,mode="driving")
 
-         weather_points = [] #THIS IS A  ARRAY WHERE EACH ELEM IS DICT CONTAINING INFO ABOUT STEPS
+             steps_in_leg = dirs_result[0]['legs'][0]['steps']
 
-         for i, step_in_leg in enumerate(steps_in_leg):
-             if i % 4 == 1 or i % 4 == 2:
-                 continue
+             weather_points = [] #THIS IS A  ARRAY WHERE EACH ELEM IS DICT CONTAINING INFO ABOUT STEPS
 
-             lat = step_in_leg['end_location']['lat']
-             lon = step_in_leg['end_location']['lng']
+             for i, step_in_leg in enumerate(steps_in_leg):
+                 if i % 4 == 1 or i % 4 == 2:
+                     continue
 
-             coords = (lat,lon)
-             rev_geo_result = rg.search(coords)
-             city_nam = rev_geo_result[0]['name']
+                 lat = step_in_leg['end_location']['lat']
+                 lon = step_in_leg['end_location']['lng']
 
-             #------------CALLING WEATHER API NOW-------------
+                 coords = (lat,lon)
+                 rev_geo_result = rg.search(coords)
+                 city_nam = rev_geo_result[0]['name']
 
-             url = 'http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&units=imperial&appid={}' # use imperial units.
-             appid = 'a91a7423c7a5ee0462575bde493b958b'
+                 #------------CALLING WEATHER API NOW-------------
 
-             weather_res_details = requests.get(url.format(lat,lon,appid)).json()
+                 url = 'http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&units=imperial&appid={}' # use imperial units.
+                 appid = 'a91a7423c7a5ee0462575bde493b958b'
 
-             weather_point = {
-                 #'city': city,
-                 'city_name': city_nam,
-                 'origin': origin_place,
-                 'destination': destination_place,
-                 'latitude': lat,
-                 'longitude': lon,
-                 'temperature': weather_res_details['main']['temp'],
-                 'pressure': weather_res_details['main']['pressure'],
-                 'humidity': weather_res_details['main']['humidity'],
-                 'description': weather_res_details['weather'][0]['description'],
-                 'icon': weather_res_details['weather'][0]['icon']
-             }
+                 weather_res_details = requests.get(url.format(lat,lon,appid)).json()
 
-             weather_points.append(weather_point) # putting weather points in weather_points list
+                 weather_point = {
+                     #'city': city,
+                     'city_name': city_nam,
+                     'origin': origin_place,
+                     'destination': destination_place,
+                     'latitude': lat,
+                     'longitude': lon,
+                     'temperature': weather_res_details['main']['temp'],
+                     'pressure': weather_res_details['main']['pressure'],
+                     'humidity': weather_res_details['main']['humidity'],
+                     'description': weather_res_details['weather'][0]['description'],
+                     'icon': weather_res_details['weather'][0]['icon']
+                 }
 
-         context = {'weather_info': json.dumps(weather_points)}
-         #return HttpResponse(weather_points)
+                 weather_points.append(weather_point) # putting weather points in weather_points list
+             for weather_point in weather_points:
+                 db_temp = MyWeatherDB(
+                                     City = weather_point['city_name'],
+                                     Origin= weather_point['origin'],
+                                     Destination= weather_point['destination'],
+                                     Latitude=weather_point['latitude'],
+                                     Longitude=weather_point['longitude'],
+                                     Temperature=weather_point['temperature'],
+                                     Pressure=weather_point['pressure'],
+                                     Humidity=weather_point['humidity'],
+                                     Description=weather_point['description'],
+                                     Icon=weather_point['icon']
+                                     )
+                 db_temp.save()
+             context = {'weather_info': json.dumps(weather_points)}
+             #return HttpResponse(weather_points)
 
-         return render(request,'MyWeather/userRequestResponse.html',context)
+             return render(request,'MyWeather/userRequestResponse.html',context)
     else:
-        return render(request,'MyWeather/userRequest.html')
+         return render(request,'MyWeather/userRequest.html')
